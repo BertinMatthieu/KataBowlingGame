2ac4085eb8854c5141bd3825e494a9496b556969
NEXEO\mbertin <bertin.matthieu.pierre.hector@gmail.com>
2018-03-08 16:16:25 +0100

    init kata bowling game

---
+++ KataBowlingGame/Bowling.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataBowlingGame
+{
+    public class Bowling
+    {
+        public int GetScore(int[] scores)
+        {
+            int score = 0;
+            int bonus = 0;
+
+            List<Tuple<int, int>> frames = GetFrames(scores);
+
+            for (int k = 0; k < 10; k++)
+            {
+                score += frames[k].Item1 + frames[k].Item2;
+
+                if (IsStrike(frames[k]))
+                {
+                    bonus = frames[k + 1].Item1 + frames[k + 1].Item2;
+                    score += bonus;
+                }
+                else if (IsSpare(frames[k]))
+                {
+                    bonus = frames[k + 1].Item1;
+                    score += bonus;
+                }
+            }
+            return score;
+        }
+
+        private static List<Tuple<int, int>> GetFrames(int[] scores)
+        {
+            List<Tuple<int, int>> frames = new List<Tuple<int, int>>();
+            int i = 0;
+            while (i < scores.Length)
+            {
+                if (scores[i] == 10)
+                {
+                    frames.Add(new Tuple<int, int>(10, 0));
+                    i++;
+                }
+                else
+                {
+                    if (i < scores.Length - 1)
+                    {
+                        frames.Add(new Tuple<int, int>(scores[i], scores[i + 1]));
+                        i += 2;
+                    }
+                    else
+                    {
+                        frames.Add(new Tuple<int, int>(scores[i], 0));
+                        i += 1;
+                    }
+                }
+            }
+            return frames;
+        }
+
+        private static bool IsStrike(Tuple<int, int> frame)
+        {
+            return frame.Item1 == 10 && frame.Item2 == 0;
+        }
+
+        private static bool IsSpare(Tuple<int, int> frame)
+        {
+            return frame.Item1 + frame.Item2 == 10 && frame.Item1 != 10;
+        }
+    }
+}
---
+++ KataBowlingGameTest/KataBowlingGameTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using KataBowlingGame;
+
+namespace KataBowlingGameTest
+{
+    [TestClass]
+    public class KataBowlingGameTest
+    {
+        [TestMethod]
+        public void When_all_roll_equal_1_then_it_scores_20()
+        {
+            //arrange
+            Bowling bowling = new Bowling();
+
+
+            //act
+            int score = bowling.GetScore(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+
+            //assert
+            Check.That(score).IsEqualTo(20);
+        }
+
+        [TestMethod]
+        public void When_strike_and_all_other_roll_equal_1_then_it_scores_30()
+        {
+            // arrange.
+            Bowling bowling = new Bowling();
+
+            // act.
+            int score = bowling.GetScore(new int[] { 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+
+            // assert.
+            Check.That(score).IsEqualTo(30);
+        }
+
+        [TestMethod]
+        public void When_Spare_and_Other_Rolls_Equal_To_1_then_it_scores_29()
+        {
+            Bowling bowling = new Bowling();
+
+            // act.
+            int score = bowling.GetScore(new int[] { 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+
+            // assert.
+            Check.That(score).IsEqualTo(29);
+        }
+
+        [TestMethod]
+        public void When_Srike_at_the_end_then_it_scores_30()
+        {
+            Bowling bowling = new Bowling();
+
+            // act.
+            int score = bowling.GetScore(new int[] { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 1, 0, 1, 0, 1, 0, 10, 5, 4 });
+
+            // assert.
+            Check.That(score).IsEqualTo(30);
+        }
+
+        [TestMethod]
+        public void When_Spare_at_the_end_then_it_scores_26()
+        {
+            Bowling bowling = new Bowling();
+
+            // act.
+            int score = bowling.GetScore(new int[] { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 1, 0, 1, 0, 1, 0, 1, 9, 5 });
+
+            // assert.
+            Check.That(score).IsEqualTo(26);
+        }
+
+    }
+}
